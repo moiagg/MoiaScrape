6456d4aa285e76613704d6add8443bc34061144c
moikapy <warrenckhan@gmail.com>
2019-04-02 19:13:26 -0400

    add file to collect sites urls

--- api/adaCheck.js
+++ api/adaCheck.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const request = require("request");
 const cheerio = require("cheerio");
 
+
 // @route GET api/auth/test
 // @desc TESTS AUTH ROUTE
 // @access Public
@@ -29,22 +30,25 @@ router.post("/scrape", (req, res) => {
 
     if (!error) {
       // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-
       const $ = cheerio.load(html);
+      require('../src/CollectUrl')($)
 
       // Finally, we'll define the variables we're going to capture
 
       let title;
       const json = {
+        errors:{},
         title: "",
         numberOfLinks: "",
         missingLinkTitle: "",
+        missingLinkText:"",
         numberOfImages: "",
         missingImagesAlt: "",
         numberOfVideo: "",
         missingVideoAlt: ""
       };
       // Functions
+      // GETS ELEMENTS THAT NEED TO BE CHECKED
       const getPageTitle = () => {
         if (
           $("head title").text() == undefined ||
@@ -57,19 +61,21 @@ router.post("/scrape", (req, res) => {
         }
         json.title = title;
       };
-
       const getNumOfLinks = () => {
         numberOfLinks = $("a").length;
         json.numberOfLinks = numberOfLinks;
       };
+
       const getNumOfImages = () => {
         numberOfImages = $("img").length;
         json.numberOfImages = numberOfImages;
       };
+
       const getNumOfVideo = () => {
         numberOfVideo = $("video").length;
         json.numberOfVideo = numberOfVideo;
       };
+      // Checks Elements For ADA Issues
       const checkLinks = () => {
         if (json.numberOfLinks !== 0) {
           console.log("We Have Links That Need Checking...");
@@ -77,11 +83,17 @@ router.post("/scrape", (req, res) => {
           $("a").filter(index => {
             const data = $("a")[index];
             const title = data.attribs.title;
+            const linkText = data.children[0].data
             if (!title) {
               missingLinkTitle = i++;
             }
+
+            if (!linkText) {
+              missingLinkText = i++;
+            }
           });
           json.missingLinkTitle = missingLinkTitle;
+          json.missingLinkText = missingLinkText;
         } else {
           console.log("There Are No Links On This Page...");
         }
@@ -93,14 +105,13 @@ router.post("/scrape", (req, res) => {
           $("img").filter(index => {
             const data = $("img")[index];
             const alt = data.attribs.alt;
-            if (alt) {
-            } else {
+            if (!alt) {
               missingImagesAlt = i++;
             }
           });
           json.missingImagesAlt = missingImagesAlt;
         } else {
-          console.log("There Are No Links On This Page...");
+          console.log("There Are No Images On This Page...");
         }
       };
       const checkVideos = () => {
@@ -110,18 +121,17 @@ router.post("/scrape", (req, res) => {
           $("video").filter(index => {
             const data = $("video")[index];
             const alt = data.attribs.alt;
-            if (alt) {
-            } else {
+            if (!alt) {
               missingVideoAlt = i++;
             }
           });
           json.missingVideoAlt = missingVideoAlt;
         } else {
-          console.log("There Are No Links On This Page...");
+          console.log("There Are No Videos On This Page...");
         }
       };
 
-
+      // init project
 
       getPageTitle();
       getNumOfLinks();
